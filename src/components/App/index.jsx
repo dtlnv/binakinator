2525eb90eabf2d6df0bdaf103fe79a778f37f01e
DmitriTelnov <=>
2020-11-02 09:47:30 +0200

    add dynamic max number

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -4,10 +4,43 @@ import Game from "../Game";
 import End from "../End";
 import "./_index.css";
 
-const initialRange = [0, 10001];
+const initialRange = [1, 10000];
+
+const getMaxSteps = (initMin, initMax) => {
+  const steps = [];
+
+  const rand = (min, max) => Math.floor(min - 0.5 + Math.random() * (max - min + 1));
+
+  const binar = (min, max, find, iteration) => {
+    steps[iteration]++;
+    const middle = Math.floor((min + max) / 2);
+
+    if (find === middle) {
+      return true;
+    }
+    if (min === max) {
+      return true;
+    }
+    if (find < middle && min < find) {
+      binar(min, middle, find, iteration);
+    }
+    if (find > middle && max > find) {
+      binar(middle, max, find, iteration);
+    }
+  };
+
+  for (let i = 0; i <= 100; i++) {
+    steps[i] = 0;
+    binar(initMin, initMax, rand(1, initMax), i);
+  }
+
+  return Math.max(...steps);
+};
 
 const App = () => {
-  const [[min, max], setRange] = useState(initialRange);
+  const [initMax, setInitMax] = useState(initialRange[1]);
+  const [[min, max], setRange] = useState([]);
+  const [maxSteps, setMaxSteps] = useState(0);
   const [step, setStep] = useState(0);
   const [state, setState] = useState("start");
   const [middle, setMiddle] = useState(0);
@@ -18,6 +51,12 @@ const App = () => {
     }
     setMiddle(Math.floor((min + max) / 2));
   }, [min, max, step, state]);
+
+  useEffect(() => {
+    if (initMax > 10) {
+      setMaxSteps(getMaxSteps(1, parseInt(initMax) + 1));
+    }
+  }, [initMax]);
 
   const lessAction = () => {
     setRange((prev) => [prev[0], middle]);
@@ -35,9 +74,21 @@ const App = () => {
     setStep(0);
   };
 
+  const startGameAction = () => {
+    setRange([1, initMax]);
+    setState("game");
+  };
+
   return (
     <div id="app-container">
-      {state === "start" && <Start startAction={() => setState("game")} />}
+      {state === "start" && (
+        <Start
+          max={initMax}
+          setInitMax={setInitMax}
+          maxSteps={maxSteps}
+          startAction={startGameAction}
+        />
+      )}
 
       {state === "game" && (
         <Game
--- src/components/Start/index.jsx
+++ src/components/Start/index.jsx
@@ -2,16 +2,28 @@ import React from "react";
 import Button from "../Button";
 import "./_index.css";
 
-const Start = ({ startAction }) => (
-  <div className="app-start">
-    <div className="start-content">
-      Pick a number between 1... and a 10 000.
-      <br />I will guess it in 13 steps maximum.
-    </div>
-    <Button onClick={startAction} keyCode={13} keyLabel="enter">
-      Go!
-    </Button>
-  </div>
-);
+const Start = ({ max, setInitMax, maxSteps, startAction }) => {
+  const rangeChangeAction = (number) => {
+    setInitMax(parseInt(number));
+  };
 
+  return (
+    <div className="app-start">
+      <div className="start-content">
+        Pick a number between 1 ... and
+        <input
+          type="number"
+          defaultValue={max ? max : 10}
+          onChange={(e) => rangeChangeAction(e.target.value, "max")}
+          title="Min 10"
+        />
+        .
+        <br />I will guess it in {maxSteps} steps maximum.
+      </div>
+      <Button onClick={startAction} keyCode={13} keyLabel="enter">
+        Go!
+      </Button>
+    </div>
+  );
+};
 export default Start;
